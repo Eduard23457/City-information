4f68750b565a6930cfebe63f04b43ed86d80c30e
Эдуард <khidiyatov2000@bk.ru>
2025-08-10 23:09:43 +0500

    Initial commit: created Spring Boot project structure

---
+++ src/main/java/org/example/cityinformation/CityInformationApplication.java
@@ -0,0 +1,13 @@
+package org.example.cityinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/cityinformation/CityInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cityinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
